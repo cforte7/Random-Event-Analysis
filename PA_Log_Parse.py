daca890f2c5d83f7a7891e0aad772d216dada738
cforte7 <cforte7@gmail.com>
2017-09-24 23:24:31 -0500

    Update PA_Log_Parse.py

--- PA_Log_Parse.py
+++ PA_Log_Parse.py
@@ -2,6 +2,7 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy import stats
 
 with open('test.txt') as f:
 	data = f.read()
@@ -19,7 +20,6 @@ for x in data:
 		if x[0] == 'type: 0' and (x[3] == 'attacker_name: npc_dota_hero_phantom_assassin' or x[4] == 'attacker_name: npc_dota_hero_phantom_assassin') and x[1].split('_')[-1] not in exclude:
 			pa.append(x)
 			dmg.append(x[11].split('value: ')[1])
-			
 			if [x[1],x[2]] not in targets:
 				targets.append([x[1],x[2]])
 			
@@ -40,17 +40,28 @@ for x in dmg:   #convert damage into bernouli trial 1=success, 0=failure
 
 
 bernstr = (''.join(bernoulli)).split('1')
+
 lengths = {}
-su = 0
-for x in bernoulli:
-	su+= int(x)
+dist = []
 
-print(su/len(bernoulli))
-for x in set(bernstr):  #construct dictionary based on observed quantites
-	lengths[len(x)] = 0
+for x in bernstr:  #construct list based on observed quantites for geometric
+	if len(x) not in lengths:
+		lengths[len(x)] = 0
+	else:
+		lengths[len(x)] += 1
+	dist.append(len(x)+1)
 
-for x in bernstr:  #record number of occurences of each observation type
-	lengths[len(x)] += 1
+
+tru_geo = np.random.geometric(.15,size=len(dist))
+tru_geo_cum = [0 for x in range(1,max(tru_geo)+1)]
+cum_tot = 0
+
+
+#plt.show()
+
+
+end = stats.ks_2samp(dist,tru_geo)
+print(end)
 
 
 
